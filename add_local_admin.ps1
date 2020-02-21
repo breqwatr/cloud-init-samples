66b1829bf8c1d37e90e7545762899e40c85b143b
Kyle Pericak <kyle@pericak.com>
2020-02-21 10:07:54 -0500

    add_local_admin sample

---
+++ add_local_admin.ps1
@@ -0,0 +1,17 @@
+function Add-LocalAdmin {
+    # Usage: Add-LocalAdmin -Name MyUsername -Password MyPassword
+    param(
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)] $Name,
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)] $Password
+    )
+    $new_user = Get-LocalUser | where {$_.name -eq $Name}
+    if ($new_user -eq $null){
+        $password_secure_string = ConvertTo-SecureString -AsPlainText -Force $Password
+        $new_user = New-LocalUser -Name $Name -Password $password_secure_string -AccountNeverExpires
+    }
+    $user_local_admin = Get-LocalGroupMember 'Administrators' | where {$_.name -Like "*\$($new_user.name)"}
+    if ($user_local_admin -eq $null){
+        Add-LocalGroupMember -Group "Administrators" -Member $new_user
+    }
+    return $new_user
+}
